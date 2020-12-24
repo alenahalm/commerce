a37480d06ce45dd3ba067002b233706ea0cd90a7
alenahalm <alenahalmatova749@gmail.com>
2020-12-24 14:21:24 +0800

    Initial commit

---
+++ commerce.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k;
+	cout << "Enter k" << endl;
+	cin >> k;
+	double p;
+	cout << "Enter percentage" << endl;
+	cin >> p;
+	int r;
+	cout << "Enter the profit" << endl;
+	cin >> r;
+	int i;
+	for (i = 0; k > 0; i++) {
+		k *= (1 + p / 100);
+		k -= r;
+	}
+	cout << i << " years" << endl;
+	system("pause");
+}
